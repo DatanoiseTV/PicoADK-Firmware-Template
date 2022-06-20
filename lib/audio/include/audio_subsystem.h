f9d42dce267d93d19b63b22413b642b1930c55cc
DatanoiseTV <DatanoiseTV.git@ext.no-route.org>
2022-06-20 14:11:36 +0200

    Modify audio lib to use pico-extras.

--- lib/audio/include/audio_subsystem.h
+++ lib/audio/include/audio_subsystem.h
@@ -5,7 +5,7 @@
 #define SAMPLES_PER_BUFFER 64 // Samples / channel
 
 #define USE_AUDIO_I2S 1
-#include "audio_i2s.h"
+#include "pico/audio_i2s.h"
 #include <stdio.h>
 
 audio_buffer_pool_t *init_audio();
