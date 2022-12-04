7979d757d64c5b8bcc024e12d533eb005e331238
Sylwester <6614616+DatanoiseTV@users.noreply.github.com>
2022-12-04 05:36:36 +0100

    Add adc128_read() and seed srand() by entropy gained from ring oscillator.

--- src/picoadk_hw.cpp
+++ src/picoadk_hw.cpp
@@ -1,6 +1,7 @@
 #include "picoadk_hw.h"
 
-void picoadk_init(){
+void picoadk_init()
+{
         // Overclock the CPU to 226 MHz.
         // We choose 226 MHz, because it works the best for the audio subsystem.
         vreg_set_voltage(VREG_VOLTAGE_1_30);
@@ -25,4 +26,47 @@ void picoadk_init(){
         // LED on GPIO15
         gpio_init(15);
         gpio_set_dir(15, GPIO_OUT);
+
+        adc_init();
+
+        // Make sure GPIO is high-impedance, no pullups etc
+        adc_gpio_init(26);
+        // Select ADC input 0 (GPIO26)
+        adc_select_input(0);
+
+        srand(adc_read());
+
+        uint32_t rand_seed;
+        for (int i = 0; i < 32; i++)
+        {
+                bool randomBit = rosc_hw->randombit;
+                rand_seed = rand_seed | (randomBit << i);
+        }
+
+        srand(rand_seed);
+
+        gpio_set_function(10, GPIO_FUNC_SPI);
+        gpio_set_function(11, GPIO_FUNC_SPI);
+        gpio_set_function(12, GPIO_FUNC_SPI);
+        gpio_init(13);
+        gpio_set_dir(13, GPIO_OUT);
+        gpio_put(13, 1);
+
+        spi_init(spi1, 16000000);
+        spi_set_format(spi1, 8, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+        spi_set_slave(spi1, false);
+}
+
+int adc128_read(uint8_t chan)
+{
+        if (chan > 7)
+                return 0;
+        gpio_put(13, 0);
+        uint8_t data[2] = {(chan << 3), 0};
+        uint8_t rxData[2];
+        spi_write_read_blocking(spi1, data, rxData, 2);
+        gpio_put(13, 1);
+        uint16_t result = (rxData[0] << 8) | rxData[1];
+
+        return result;
 }
