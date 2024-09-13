fe71053e1b73504d3ed950d9c71ab6fb5176cb9d
DatanoiseTV <github@ext.no-route.org>
2024-09-13 18:59:56 +0200

    Minor code optimization.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "project_config.h"
 
-
 // Reverse compatibility with old PicoSDK.
 #if __has_include("bsp/board_api.h")
 #include "bsp/board_api.h"
@@ -20,60 +19,58 @@
 #include "arduino_compat.h"
 
 // Audio Buffer (Size is set in lib/audio/include/audio_subsystem.h)
-audio_buffer_pool_t *ap;
+audio_buffer_pool_t *audio_pool;
 
-MIDIInputUSB usbmidi;
+MIDIInputUSB usbMIDI;
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
-    // MIDI callbacks
-
-    void note_off_callback(uint8_t note, uint8_t vel, uint8_t channel)
-    {
-        // put your node off code for the dsp engine here
-        // e.g. Dsp_noteOff(ctx, note, channel);
-    }
-
-    void note_on_callback(uint8_t note, uint8_t vel, uint8_t channel)
-    {
-        if (vel > 0)
-        {
-            // put your node on code for the dsp engine here
-            // e.g. Dsp_noteOn(ctx, note, channel);
-        }
-        else
-        {
-            note_off_callback(note, vel, channel);
-        }
-    }
-
-    void cc_callback(uint8_t cc, uint8_t value, uint8_t channel)
-    {
-        // put your cc code for the dsp engine here
-        // e.g. Dsp_cc(ctx, cc, value, channel);
-    }
-
-    // This task processes the USB MIDI input
+    /**
+     * Task to handle USB MIDI input processing.
+     *
+     * @param pvParameters Unused task parameters
+     */
     void usb_midi_task(void *pvParameters)
     {
+        // Setup MIDI Callbacks using lambdas
+        usbMIDI.setCCCallback([](uint8_t cc, uint8_t value, uint8_t channel) {
+            // Handle Control Change (CC) event
+            // e.g. Dsp_cc(ctx, cc, value, channel);
+        });
 
-        // Basic Callbacks for MIDI Notes & CCs
-        usbmidi.setCCCallback(cc_callback);
-        usbmidi.setNoteOnCallback(note_on_callback);
-        usbmidi.setNoteOffCallback(note_off_callback);
+        usbMIDI.setNoteOnCallback([](uint8_t note, uint8_t velocity, uint8_t channel) {
+            if (velocity > 0)
+            {
+                // Handle Note On event
+                // e.g. Dsp_noteOn(ctx, note, channel);
+            }
+            else
+            {
+                // Treat zero velocity as Note Off
+                // e.g. Dsp_noteOff(ctx, note, channel);
+            }
+        });
+
+        usbMIDI.setNoteOffCallback([](uint8_t note, uint8_t velocity, uint8_t channel) {
+            // Handle Note Off event
+            // e.g. Dsp_noteOff(ctx, note, channel);
+        });
 
         while (1)
         {
             // TinyUSB Device Task
             tud_task();
-            usbmidi.process();
+            usbMIDI.process();
         }
     }
 
-    // This task blinks the LED on GPIO 2
+    /**
+     * Task to blink an LED on GPIO pin 2.
+     *
+     * @param pvParameters Unused task parameters
+     */
     void blinker_task(void *pvParameters)
     {
         gpio_init(2);
@@ -82,62 +79,64 @@ extern "C"
         while (1)
         {
             gpio_put(2, 1);
-            vTaskDelay(pdMS_TO_TICKS(100));
+            vTaskDelay(pdMS_TO_TICKS(100)); // Delay for 100ms
             gpio_put(2, 0);
         }
     }
 
+    /**
+     * Main entry point.
+     */
     int main(void)
     {
-        // initialize the hardware
+        // Initialize hardware
         picoadk_init();
 
-        // Initialize your DSP engine here
+        // Initialize DSP engine (if needed)
 
         // Initialize the audio subsystem
-        ap = init_audio();
+        audio_pool = init_audio();
 
-        // Create FreeRTOS Tasks for USB MIDI and printing statistics
-        xTaskCreate(usb_midi_task, "USBMIDI", 4096, NULL, configMAX_PRIORITIES, NULL);
-        xTaskCreate(blinker_task, "BLINKER", 128, NULL, configMAX_PRIORITIES - 1, NULL);
+        // Create FreeRTOS tasks for MIDI handling and LED blinking
+        xTaskCreate(usb_midi_task, "USB_MIDI_Task", 4096, NULL, configMAX_PRIORITIES, NULL);
+        xTaskCreate(blinker_task, "Blinker_Task", 128, NULL, configMAX_PRIORITIES - 1, NULL);
 
-        // Start the scheduler.
+        // Start the FreeRTOS scheduler
         vTaskStartScheduler();
 
-        // Idle loop.
+        // Idle loop (this is fine for Cortex-M33)
         while (1)
         {
-            ;
-            ;
+            // Could use `taskYIELD()` or similar here if needed
         }
     }
 
-    // This fis the I2S callback function. It is called when the I2S subsystem
-    // needs more audio data. It is called at a fixed rate.
-    // The audio data is stored in the audio_buffer_t struct.
-    //
-    // !!!! Please note that this function needs to take less to execute than the time
-    // between two calls to this function. Otherwise the audio will be interrupted. !!!!
-
+    /**
+     * I2S audio callback for filling the audio buffer with samples.
+     *
+     * This function is called at a fixed rate determined by the audio subsystem
+     * and must return within the interval between calls to avoid audio glitches.
+     */
     void __not_in_flash_func(i2s_callback_func())
     {
-        audio_buffer_t *buffer = take_audio_buffer(ap, false);
+        audio_buffer_t *buffer = take_audio_buffer(audio_pool, false);
         if (buffer == NULL)
         {
             return;
         }
+
         int32_t *samples = (int32_t *)buffer->buffer->bytes;
-        // We are filling the buffer with 32-bit samples (2 channels)
+
+        // Fill buffer with 32-bit samples (stereo, 2 channels)
         for (uint i = 0; i < buffer->max_sample_count; i++)
         {
-            samples[i * 2 + 0] = 0;   // Use your DSP function here to output the L sample
-            samples[i * 2 + 1] = 0;   // Use your DSP function here to output the R sample
-
+            samples[i * 2 + 0] = 0;   // Left channel sample
+            samples[i * 2 + 1] = 0;   // Right channel sample
+            // Use your DSP function here for generating the audio samples
         }
 
         buffer->sample_count = buffer->max_sample_count;
-        give_audio_buffer(ap, buffer);
-        return;
+        give_audio_buffer(audio_pool, buffer);
     }
 
 #ifdef __cplusplus
